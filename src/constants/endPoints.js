f80cb593df11966ff8135be073d3f24eb7556d5b
Salman Irfan <salmanirfan674@gmail.com>
2025-05-06 14:08:25 +0500

    refactoring in api urls

--- src/constants/endPoints.js
+++ src/constants/endPoints.js
@@ -1,7 +1,7 @@
 // constants/endPoints.js
 
 // Base URL (client-side only)
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "";
+// export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "";
 
 // Common Path Prefixes
 export const API = "/api";
@@ -9,23 +9,23 @@ export const V1 = "/v1";
 export const API_V1_PREFIX = `${API}${V1}`;
 
 // Full API base path
-export const API_URL = `${BASE_URL}${API_V1_PREFIX}`;
+// export const API_URL = `${BASE_URL}${API_V1_PREFIX}`;
 
 // Final structured endpoint object
 export const END_POINTS = {
   AUTH: {
-    SIGN_UP: `${API_URL}/auth/signup`,
+    SIGN_UP: `${API_V1_PREFIX}/auth/signup`,
   },
 
   CREATOR: {
-    UPLOAD_POST: `${API_URL}/creator/upload-post`,
+    UPLOAD_POST: `${API_V1_PREFIX}/creator/upload-post`,
   },
 
   MEDIA: {
-    UPLOAD_POST: `${API_URL}/creator/upload-post`,
-    FEED: `${API_URL}/media/feed`,
-    LIKE: `${API_URL}/media/like`,
-    SEARCH: `${API_URL}/media/search`,
-    COMMENT: `${API_URL}/media/comment`,
+    UPLOAD_POST: `${API_V1_PREFIX}/creator/upload-post`,
+    FEED: `${API_V1_PREFIX}/media/feed`,
+    LIKE: `${API_V1_PREFIX}/media/like`,
+    SEARCH: `${API_V1_PREFIX}/media/search`,
+    COMMENT: `${API_V1_PREFIX}/media/comment`,
   },
 };
